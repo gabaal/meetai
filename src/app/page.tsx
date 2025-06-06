9f84898693816f82f894d2a7a59d4c2e91a6f5e3
Gerald Baalham <geraldbaalham@live.co.uk>
2025-06-06 08:08:31 +0100

    feat: implement social authentication with GitHub and Google, add HomeView component, and update sign-in/sign-up views

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,26 @@
-'use client'
+import { headers } from "next/headers";
 
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { authClient } from "@/lib/auth-client";
-
-export default function Home() {
-
-  const { data: session } = authClient.useSession() 
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { redirect } from "next/navigation";
 
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [name, setName] = useState("")
 
-    const onSubmit = () => {
-      authClient.signUp.email({
-        email,
-        password,
-        name,
-      }, {
-        onError: () => {
-          window.alert("Error creating user")
-        },
-        onSuccess: () => {
-          window.alert("User created successfully")
-        }
-      })
-    }
+const Page = async () => {
 
-    if (session) {
-      return (
-        <div className="flex flex-col p-4 gap-y-4"><p>Logged in as {session.user.name}</p>
-        <Button onClick = {() => authClient.signOut()}>Sign Out</Button></div>
-      )
-    }
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!session) {
+    redirect("/sign-in")
+  }
 
   return (
-    <div className="p-4 flex flex-col gap-y-4">
-      <Input placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
-      <Input placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <Input placeholder="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-
-    <Button onClick={onSubmit}>Create User</Button>
-
+    <div>
+      <HomeView />
     </div>
   );
 }
+
+export default Page;
