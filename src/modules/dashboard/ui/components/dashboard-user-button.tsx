9531ab09b1a339116b145808172462ea590abbe9
Gerald Baalham <geraldbaalham@live.co.uk>
2025-06-06 13:31:38 +0100

    fix: add non-null assertion for user data in DashboardUserButton component

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -31,11 +31,11 @@ export const DashboardUserButton = () => {
                     <Avatar>
                         <AvatarImage src={data.user.image} alt="User Avatar" />
                     </Avatar>
-                ) : <GeneratedAvatar seed={data.user.name} variant="initials" className="size-9 mr-3" />}
+                ) : <GeneratedAvatar seed={data!.user.name} variant="initials" className="size-9 mr-3" />}
 
                 <div className="flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0">
-                    <p className="text-sm truncate w-full">{data.user.name}</p>
-                    <p className="text-xs truncate w-full">{data.user.email}</p>
+                    <p className="text-sm truncate w-full">{data!.user.name}</p>
+                    <p className="text-xs truncate w-full">{data!.user.email}</p>
                 </div>
                 <ChevronDownIcon className="size-4 shrink-0" />
             </DropdownMenuTrigger>
@@ -43,8 +43,8 @@ export const DashboardUserButton = () => {
             <DropdownMenuContent align='end' side='right' className="w-72">
                 <DropdownMenuLabel>
                     <div className="flex flex-col gap-1">
-                        <span className="font-medium truncate">{data.user.name}</span>
-                        <span className="text-xs font-normal text-muted-foreground truncate">{data.user.email}</span>
+                        <span className="font-medium truncate">{data!.user.name}</span>
+                        <span className="text-xs font-normal text-muted-foreground truncate">{data!.user.email}</span>
                     </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
