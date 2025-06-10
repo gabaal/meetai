945bb1db74841f41e5e3d27267d8d8a26c12f066
Gerald Baalham <geraldbaalham@live.co.uk>
2025-06-10 13:27:19 +0100

    feat: add meetings management features, including schema, procedures, and UI components

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
 
 import { agentsRouter } from '@/modules/agents/servers/procedures';
 import { createTRPCRouter } from '../init';
+import { meetingsRouter } from '@/modules/meetings/server/procedures';
 
 
 
 export const appRouter = createTRPCRouter({
-  agents: agentsRouter
+  agents: agentsRouter,
+  meetings: meetingsRouter
 });
 
 export type AppRouter = typeof appRouter;
