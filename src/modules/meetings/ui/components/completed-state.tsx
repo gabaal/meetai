2c9a242f67bfb7ffbbd6495b6a998eca7de83415
Gerald Baalham <geraldbaalham@live.co.uk>
2025-06-16 14:41:45 +0100

    feat: add transcript feature with search functionality and integrate it into completed state

--- src/modules/meetings/ui/components/completed-state.tsx
+++ src/modules/meetings/ui/components/completed-state.tsx
@@ -8,6 +8,7 @@ import { GeneratedAvatar } from "@/components/generated-avatar"
 import { format } from "date-fns"
 import { Badge } from "@/components/ui/badge"
 import { formatDuration } from "@/lib/utils"
+import { Transcript } from "./transcript"
 interface Props {
     data: MeetingGetOne
 }
@@ -53,6 +54,10 @@ export const CompletedState = ({ data }: Props) => {
                         <ScrollBar orientation="horizontal" />
                     </ScrollArea>
                 </div>
+
+<TabsContent value = 'transcript'>
+    <Transcript meetingId = {data.id}/>
+</TabsContent>
 
                 <TabsContent value='recording'>
                     <div className="bg-white rounded-lg border px-4 py-5">
